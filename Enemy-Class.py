b91593f691fb3ba1833276720d7709cd9e963005
heberbeeper <heber42800@gmail.com>
2021-05-27 11:55:06 -0600

    first enemy class attempt

---
+++ Enemy-Class.py
@@ -0,0 +1,61 @@
+#List of all the Enemy Classes
+#Separated by fire nation, creatures, pirates, ect. 
+
+
+"""
+class Enemy:
+
+    def __init__(self, name, health, damage, defense, damage_focus):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defense = defense 
+        self.damage_focus = damage_focus
+
+    def stats(self):
+        return 
+    
+    Enemy_1 = wolfbat()
+    Enemy_2 = Fire_Nation_Soldier()
+    Enemy_3 = Tundra_Tank()
+"""
+
+
+class Enemy:
+
+    def __init__(self, ):
+
+    
+    self.name = ""
+
+    self.health = 0
+    self.damage = 0
+    self.defense = 0
+    self.damage_focus = 0
+    self.strength = 0
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
