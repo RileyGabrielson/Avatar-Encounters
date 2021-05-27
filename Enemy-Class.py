63a586214aae1415ec7a6e767d3b977f914375d0
Riley Gabrielson <rileygabrielson@gmail.com>
2021-05-27 12:09:09 -0600

    fixed tab error on enemy class

--- Enemy-Class.py
+++ Enemy-Class.py
@@ -1,25 +1,22 @@
 #List of all the Enemy Classes
 #Separated by fire nation, creatures, pirates, ect. 
 
-
-
 class Enemy:
 
     def __init__(self, ):
+        self.name = ""
+        self.category = ""
+        self.level = ""
 
-    self.name = ""
-    self.category = ""
-    self.level = ""
+        self.health = 0
+        self.damage = 0
+        self.defense = 0
+        self.pierce = 0
 
-    self.health = 0
-    self.damage = 0
-    self.defense = 0
-    self.pierce = 0
-
-    self.singleTarget = False
-    self.giveStrength = False
-    self.targetHighest = False
-    self.targetLowest = False
+        self.singleTarget = False
+        self.giveStrength = False
+        self.targetHighest = False
+        self.targetLowest = False
 
     
 
